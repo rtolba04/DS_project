0aa88f3f8439b90fc8eb88b0b0bf13607238805c
Afify21 <lolesea21@gmail.com>
2024-04-12 06:51:37 +0200

    s

--- DS_project/alienArmy.h
+++ DS_project/alienArmy.h
@@ -1,5 +1,23 @@
 #pragma once
+
+#include"Aliensoldiers.h"
+#include"Aliendrones.h"
+#include"Alienmonsters.h"
+#include"LinkedQueue.h"
+#include"ArrayStack.h"
+#include"priQueue.h"
+#include"Unitclass.h"
+#include "ArrBag.h"
 class alienArmy
 {
+private:
+	LinkedQueue<Aliensoldiers*>ASqueue;
+	ArrBag<Alienmonsters*>AMarray;
+	//<Aliendrones*>; ADqueue;
+public:
+	void attack();
+	void addUnit(Unitclass* unit);
+	void removeUnit(Unitclass* unit);
+	void print();
 };
 

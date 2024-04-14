7191cbde74209f86b8e74eb0dfd3eeaf9e2af4dd
Afify21 <lolesea21@gmail.com>
2024-04-14 12:06:32 +0200

    a

--- DS_project/Game.cpp
+++ DS_project/Game.cpp
@@ -8,27 +8,55 @@
 using namespace std;
 
 int main()
-{
-	int N, Prob, ESpr, ETpr, EGpr, ASpr, AMpr, ADpr, A, B;
+{// for one timestep
+	int N, Prob, ESpr, ETpr, EGpr, ASpr, AMpr, ADpr,Ep,Eh,Ea,Ap,Ah,Ac, A, B,eminrangepower,emaxrangepower, eminrangehealth, emaxrangehealth;
+	int eminrangeeac, emaxrangeac, aminrangepower, amaxrangepower,aminrangehealth, amaxrangehealth, aminrangeeac, amaxrangeac
 	A = rand() % 100 + 1;
 	cout << "Random number between 1 and 100: " << A << endl;
 	ifstream input("inputfile.txt");
 	input >> N;
-	input >> Prob;
-	if (A <= Prob) {
-		B = rand() % 100 + 1;
-		//cout << "Random number between 1 and 100: " << B << endl;
-		for (int i = 0; i < N; i++) {
-			earthArmy EA1;
-			Alienarmy AA1;
-		}
-	}
 	input >> ESpr;
 	input >> ETpr;
 	input >> EGpr;
 	input >> ASpr;
 	input >> AMpr;
 	input >> ADpr;
+	input >> Prob;
+	input >> eminrangepower;
+	input >> emaxrangepower;
+	
+	input >> eminrangehealth;
+	input >> emaxrangehealth;
+	
+	input >> eminrangeeac;
+	input >> emaxrangeac;
+	
+	input >> aminrangepower;
+	input >> amaxrangepower;
+
+	input >> aminrangehealth;
+	input >> amaxrangehealth;
+	
+	input >> aminrangeeac;
+	input >> amaxrangeac;
+	
+
+	if (A <= Prob) {
+		B = rand() % 100 + 1;
+		//cout << "Random number between 1 and 100: " << B << endl;
+		for (int i = 0; i < N; i++) {
+			earthArmy EA1;
+			alienArmy AA1;
+			Ep = rand() % (emaxrangepower - eminrangepower + 1) + eminrangepower;
+			Eh = rand() % (emaxrangehealth - eminrangehealth + 1) + eminrangehealth;
+			Ea = rand() % (emaxrangeac - eminrangeeac + 1) + eminrangeeac;
+			Ap = rand() % (amaxrangepower - aminrangepower + 1) + aminrangepower;
+			Ah = rand() % (amaxrangehealth - aminrangehealth + 1) + aminrangehealth;
+			Ac = rand() % (amaxrangeac - aminrangeeac + 1) + aminrangeeac;
+			EA1
+		}
+	}
+	
 
 
 	input.close();
@@ -40,6 +68,9 @@ int main()
 	cout << "Loading ASpr from input file: " << ASpr << endl;
 	cout << "Loading AMpr from input file: " << AMpr << endl;
 	cout << "Loading ADpr from input file: " << ADpr << endl;
+	cout << "Loading health  of es from input file: " << Eh << endl;
+	cout << "Loading power pf es from input file: " << Ep << endl;
+
 
 	return 0;
 }

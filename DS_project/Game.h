f026f4b439d4c800728a6c5087f78ec6639c8298
Ruaa <Ruaa@DESKTOP-RAEOIB2>
2024-04-25 17:38:06 +0200

    s

--- DS_project/Game.h
+++ DS_project/Game.h
@@ -1,7 +1,20 @@
 #pragma once
+#include "LinkedQueue.h"
+#include"Unitclass.h"
+#include "randGen.h"
+#include "earthArmy.h"
+#include "alienArmy.h"
 class Game
-{private:
-	
+{
+private:
+	int time;
+	randGen randomgen; //randgen obj
+	earthArmy Earth;//Earth obj
+	alienArmy Alien;//alien obj
+	LinkedQueue<Unitclass*>* killed;
+	LinkedQueue<Unitclass*>* temp;
+
+
 public:
 	Game();
 	randGen* getrand();
@@ -11,6 +24,7 @@ public:
 	void PrintKilledList();
 
 	void kill(Unitclass* unit);
+	void printKill();
 	void test();
 	//void testnew();
 	~Game();

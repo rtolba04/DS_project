435665b1d15ddcf30b4088be2e3bcb11fcdc6779
Habiba Mamdouh <Habiba Mamdouh@LAPTOP-P0JPCHNC>
2024-05-12 23:42:27 +0300

    edited heal, still needs timejoined UML

--- DS_project/Health.cpp
+++ DS_project/Health.cpp
@@ -8,19 +8,22 @@ Health::Health(int id, int jt, int h , int p, int ac) :Unitclass(id, jt, h, p, a
 }
 void Health::attack()
 {
+	Health* HU;
 	LinkedQueue<Unitclass*>* temp = nullptr;
-	while (Attackcapacity != 0) {
+	while (HU->Getattackcapacity() != 0) {
 		if (ptr->UMLsoldiers.getHead())
 		{
 			int pri = -GetHealth();
 			Earthsoldiers* es = ptr->UMLsoldiers.getHead()->getItem(pri);
-			if (ptr->getTime() - es->Getjointime() > 10) {
+			if (ptr->getTime() - es->Getjointime() > 10) //edit to time join UML
+			{
 				ptr->kill(es);
 			}
 			else {
 				int oldhealth = es->GetHealth();
-				int healthimp = (Power * GetHealth() / 100) / sqrt(oldhealth);
-				SetHealth(healthimp + GetHealth());
+				int healthimp = (HU->GetPower() * HU->GetHealth() / 100) / sqrt(oldhealth);
+				es->SetHealth(healthimp + oldhealth);
+				HU->Setattackcapacity(Attackcapacity - 1);
 				if (es->GetHealth() > (oldhealth)*20/100) {
 					ptr->getEA()->addUnit(es);
 				}
@@ -32,14 +35,15 @@ void Health::attack()
 		}
 		else if(ptr->UMLtanks.getfrontPtr()) {
 			Earthtanks* et = ptr->UMLtanks.getfrontPtr()->getItem();
-			if (ptr->getTime() - et->Getjointime() > 10)
+			if (ptr->getTime() - et->Getjointime() > 10) // edit so it is time joined UML
 			{
 				ptr->kill(et);
 			}
 			else {
 				int oldhealth = et->GetHealth();
-				int healthimp = (Power * GetHealth() / 100) / sqrt(oldhealth);
-				SetHealth(healthimp + GetHealth());
+				int healthimp = (HU->GetPower() * HU->GetHealth() / 100) / sqrt(oldhealth);
+				et->SetHealth(healthimp + oldhealth);
+				HU->Setattackcapacity(Attackcapacity - 1);
 				if (et->GetHealth() > (oldhealth) * 20 / 100) {
 					ptr->getEA()->addUnit(et);
 				}
@@ -64,5 +68,7 @@ void Health::attack()
 			ptr->UMLtanks.enqueue(et);
 		}
 	}
+
+	delete HU;
 	//3ayzin n delete HU HOW???
 }
